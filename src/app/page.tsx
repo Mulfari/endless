8528b06d3368416270064fdd87255efd3caea2cb
Salvador <josesalvador0102@gmail.com>
2025-07-19 16:12:39 +0100

    up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@
 
 import Link from "next/link";
 import { useEffect, useState } from "react";
-import Image from "next/image";
 import HeroSection from "../components/HeroSection";
 import ExperienciasSection from "../components/ExperienciasSection";
 import GaleriaSection from "../components/GaleriaSection";
